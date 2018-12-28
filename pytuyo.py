5fc416edbf3ff96cfa8c86a9bb5cb97a413788f4
SNUC User <snuc@lucidsci.com>
2018-12-28 11:06:02 -0500

    usb reads with timeout value for performance boost
    - without specifying timeout, we must block for mitutoyo response
    which slows down rest of program

--- pytuyo.py
+++ pytuyo.py
@@ -2,18 +2,27 @@ import usb
 import logging as log
 import time
 import argparse
+from collections import deque
 
 CMD_TERMINATOR = b'\r'
-MAX_RESP=64 #bytes
+MSG_TERMINATOR = b'\r'
+READ_TIMEOUT_MS = 10
+MAX_RXQUEUE_LEN=1024
+
+DATA_MSG='0'
+DEVICE_INFO_MSG='1'
+STATUS_MSG='9'
 
 class Pytuyo(object):
     def __init__(self, usb_dev):
         self._usb_dev = usb_dev
         self._epin = None
-        
+        self._rxqueue = deque(maxlen=MAX_RXQUEUE_LEN)
+
         self.data_cb = None
         self.device_info_cb = None
         self.status_cb = None
+        self._waiting_resp = False
 
         self.setup()
 
@@ -21,11 +30,11 @@ class Pytuyo(object):
         d = self._usb_dev
         if d.is_kernel_driver_active(0):
             d.detach_kernel_driver(0)
-        
+
         d.reset()
         d.set_configuration(1)
         c = d.get_active_configuration()
-        self._epin = d.get_active_configuration().interfaces()[0].endpoints()[0]  
+        self._epin = d.get_active_configuration().interfaces()[0].endpoints()[0]
         bmRequestType=0x40 # Vendor Host-to-Device
         bRequest=0x01
         wValue=0xA5A5
@@ -39,8 +48,12 @@ class Pytuyo(object):
         length=1
         res1 = d.ctrl_transfer(bmRequestType, bRequest, wValue, wIndex, length)
         log.debug("Device Vendor resp: {}".format(res1))
-    
+
     def send_cmd(self, cmd):
+        if self._waiting_resp:
+            log.warning("Cannot send mitutuyo cmd - still waiting response")
+            return
+
         if not isinstance(cmd, bytes):
             try:
                 cmd = cmd.encode()
@@ -49,91 +62,117 @@ class Pytuyo(object):
 
         if cmd[-1] != CMD_TERMINATOR:
             cmd+=CMD_TERMINATOR
-        
+
         bmRequestType=0x40 # Vendor Host-to-Device
         bRequest=0x03
         try:
             self._usb_dev.ctrl_transfer(bmRequestType, bRequest, 0, 0, cmd)
         except usb.USBError as e:
             log.error(str(e))
-    
+
+        self._waiting_resp = True
+
     def request_read(self):
         self.send_cmd('1')
-    
+
     def request_device_info(self):
         self.send_cmd('V')
-    
+
     def _process_data_resp(self, response):
         MIN_DATA_LEN=4
         if len(response) < MIN_DATA_LEN:
             log.error("Invalid data measurement resp '{}'".format(response))
             return
-    
+
         #ignore first two characters - always "1A"
-        measure_str = response[2:] 
-    
+        measure_str = response[2:]
+
         try:
             val = float(measure_str)
         except ValueError as e:
             log.error("Unable to parse measurement '{}' to float".format(measure_str))
             return
-        
+
         log.debug("Received measure data value: {}".format(val))
 
         if self.data_cb: self.data_cb(val)
-    
+
     def _process_device_info_resp(self, response):
         log.debug("Received device info msg : {}".format(response))
 
         if self.device_info_cb: self.device_info_cb(response)
-    
+
     def _process_status_resp(self, response):
         log.debug("Received device status msg : {}".format(response))
 
         if self.status_cb: self.status_cb(response)
-        
-        
-    def check_resp(self):
+
+
+    def _rx(self):
         if self._epin is None:
-            raise Exception("Device not setup correcly for reading - no interrupt IN endpoint")
+            raise Exception("Device not setup correctly for reading - no interrupt IN endpoint")
         try:
-            resp = self._epin.read(MAX_RESP)
+            max_rx = self._epin.wMaxPacketSize
+            resp = self._epin.read(max_rx, READ_TIMEOUT_MS)
 
             if not resp or len(resp) == 0:
                 return
-            
-            DATA_MSG='0'
-            DEVICE_INFO_MSG='1'
-            STATUS_MSG='9'
 
-            resp = resp.tobytes().decode()
-            cmd_c = resp[0]
-            if cmd_c == DATA_MSG:
-                self._process_data_resp(resp[1:])
-            elif cmd_c == DEVICE_INFO_MSG:
-                self._process_device_info_resp(resp[1:])
-            elif cmd_c == STATUS_MSG:
-                self._process_status_resp(resp[1:])
-            else:
-                log.error("Ignoring unexpected device resp {}".format(resp))
+            self._rxqueue.extend(resp)
+
         except usb.USBError as e:
             if e.errno == 110:
                 log.debug("USB timeout waiting for response")
-                return None
+                return
             else:
                 raise
+
+    def check_resp(self):
+        self._rx()
+
+        if len(self._rxqueue) == 0:
+            return
+
+        rxdata = bytes(self._rxqueue)
+        eor_idx = rxdata.find(MSG_TERMINATOR)
+        if eor_idx == -1:
+            """ message terminator not received yet"""
+            return
+
+        resp = rxdata[:eor_idx]
+
+        self._rxqueue.clear()
+        if len(rxdata) >= eor_idx:
+            """ add back any data that will not be processed"""
+            rem = resp[eor_idx+1:]
+            self._rxqueue.extend(rem)
+
+        self._waiting_resp = False
+
+        resp = resp.decode()
+        msg_c = resp[0]
+        if msg_c == DATA_MSG:
+            self._process_data_resp(resp[1:])
+        elif msg_c == DEVICE_INFO_MSG:
+            self._process_device_info_resp(resp[1:])
+        elif msg_c == STATUS_MSG:
+            self._process_status_resp(resp[1:])
+        else:
+            log.error("Ignoring unexpected device resp {}".format(resp))
+
+        return resp
 
 def make_parser():
     """ create the argument parser """
     parser = argparse.ArgumentParser(description="Interact with Mitutoyo USB-ITN with pyusb")
-    
+
     parser.add_argument('-i', '--request-device-info', type=bool, default=True,
             help='request device info')
     parser.add_argument('-n', '--read-count', type=int, default=1,
             help='Read count. -1 for inf')
     parser.add_argument('-t', '--read-interval', type=float, default=1,
             help='Read interval in seconds')
-    
+
     return parser
 
 if __name__ == '__main__':
@@ -142,14 +181,14 @@ if __name__ == '__main__':
     log.basicConfig(level=log.INFO)
     parser = make_parser()
     args = parser.parse_args()
-    
+
     d = usb.core.find(idVendor=0x0fe7, idProduct=0x4001)
     if d is None:
         print("Could not find USB-ITN (idVendor=0x0fe7, idProduct=0x4001)")
         sys.exit(1)
 
     p = Pytuyo(d)
-    
+
     p.data_cb = lambda v: print("M:{}".format(v))
     p.device_info_cb = lambda v: print("Device Info: {}".format(v))
     p.status_cb = lambda v: print("Device Status: {}".format(v))
@@ -157,14 +196,15 @@ if __name__ == '__main__':
     if args.request_device_info:
         p.request_device_info()
         p.check_resp()
-    
+
     n = args.read_count
 
     while True:
         p.request_read()
-        p.check_resp()
+        while not p.check_resp():
+            pass
+
         n = n - 1
         if n == 0:
             sys.exit(0)
         time.sleep(args.read_interval)
-
